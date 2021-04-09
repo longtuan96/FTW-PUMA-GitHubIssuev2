2688f6d5d65e33fd4206becba493306ed338727f
Tuan <longtuan03@gmail.com>
2021-04-09 12:59:47 +0700

    setup url for get issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 function App() {
-  return <div>alo alo</div>;
+  console.log(API_KEY);
+  const [user, setUser] = useState("facebook");
+  const [repo, setRepo] = useState("react");
+  // const octokit = new Octokit({ auth: `${API_KEY}` });
+  const getData = async () => {
+    let url = `https://api.github.com/repos/${user}/${repo}/issues?state=all`;
+    let res = await fetch(url);
+    let data = await res.json();
+    // let res = await octokit.request("GET /issues");
+    console.log("data: ", data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return <div></div>;
 }
 
 export default App;
