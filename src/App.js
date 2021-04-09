210b854f08ee70d5af9684a44a92a2f2bc8f43fa
Tuan <longtuan03@gmail.com>
2021-04-09 13:52:35 +0700

    save

--- src/App.js
+++ src/App.js
@@ -7,16 +7,24 @@ function App() {
   console.log(API_KEY);
   const [user, setUser] = useState("facebook");
   const [repo, setRepo] = useState("react");
+  const [issuesId, setIssuesId] = useState(21209);
   // const octokit = new Octokit({ auth: `${API_KEY}` });
-  const getData = async () => {
+  const getIssues = async () => {
     let url = `https://api.github.com/repos/${user}/${repo}/issues?state=all`;
     let res = await fetch(url);
     let data = await res.json();
     // let res = await octokit.request("GET /issues");
     console.log("data: ", data);
   };
+  const getComments = async () => {
+    let url = `https://api.github.com/repos/${user}/${repo}/issues/21209/comments`;
+    let res = await fetch(url);
+    let data = await res.json();
+    // let res = await octokit.request("GET /issues");
+    console.log("data: ", data);
+  };
   useEffect(() => {
-    getData();
+    getIssues();
   }, []);
   return <div></div>;
 }
