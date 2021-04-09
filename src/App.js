59949b5c45566d5ed4ceb338f9d121af28f81c81
nvak1999 <76765929+nvak1999@users.noreply.github.com>
2021-04-09 13:50:44 +0700

    search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Nav_Header from "./components/Nav_Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from "react-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faCheckSquare, faCoffee);
 
 function App() {
-  return <div>alo alo</div>;
+  return (
+    <div>
+      <Nav_Header />
+    </div>
+  );
 }
 
 export default App;
