701b9619fc536a847016dfde1d15168a80773862
Nhat Lam <nnhatlam1211@Nhats-MacBook-Pro.local>
2021-04-10 17:57:09 +0700

    Pagination 2nd ver

--- src/App.js
+++ src/App.js
@@ -12,24 +12,30 @@ function App() {
   const [repo, setRepo] = useState("react");
   const [issuesId, setIssuesId] = useState(21209);
   const [data, setdata] = useState([]);
+  // State tui add them vao ---------->
   const [currentpage, setcurrentpage] = useState(1); 
-  const [perpage] = useState(9); // perpage is const, no need setperpage
+  const [perpage] = useState(7); // perpage is const, no need setperpage
   const [totalpagenum, setTotalpagenum] = useState();
-
+  const [pagenumlimit, setPagenumlimit] = useState(5);
+  const [maxpagenumlimit, setMaxpagenumlimit] = useState(5);
+  const [minpagenumlimit, setMinpagenumlimit] = useState(0);
+  //---------->
   const getIssues = async () => {
     let url = `https://api.github.com/repos/${user}/${repo}/issues?page=${currentpage}&per_page=${perpage}`;
     let res = await fetch(url);
     let data = await res.json();
+    setdata(data)
     console.log("data: ", data);
   };
   const getComments = async () => {
     let url = `https://api.github.com/repos/${user}/${repo}/issues/21209/comments`;
     let res = await fetch(url);
     let data = await res.json();
+    setdata(data)
     console.log("data: ", data);
   };
 
- // Get totalnumber of page based on perpage
+ // Get totalnumber of page based on perpage ---------->
   const getEntirepage = async () =>{
     let url = `https://api.github.com/repos/${user}/${repo}/issues?page=${currentpage}&per_page=${perpage}`;
     let res = await fetch(url);
@@ -47,7 +53,22 @@ function App() {
     getEntirepage();
   }, [currentpage]);
 
-  // Change page
+  // truncate content
+  const text_truncate = (str, length, ending) => {
+    if (length == null) {
+      length = 100;
+    }
+    if (ending == null) {
+      ending = "...";
+    }
+    if (str.length > length) {
+      return str.substring(0, length - ending.length) + ending;
+    } else {
+      return str;
+    }
+  };
+
+  // Change page ----------->
   const paginate =(number)=>{
     console.log(number)
     setcurrentpage(number)
@@ -55,18 +76,27 @@ function App() {
   }
   const decrease = ()=>{
     setcurrentpage((currentpage)=>--currentpage)
+    if((currentpage -1)%pagenumlimit === 0){
+      setMaxpagenumlimit(maxpagenumlimit - pagenumlimit);
+      setMinpagenumlimit(minpagenumlimit - pagenumlimit);
+    }
     console.log(currentpage)
   }
 
   const increase = ()=>{
     setcurrentpage((currentpage)=>++currentpage)
+    if(currentpage +1 > maxpagenumlimit){
+      setMaxpagenumlimit(maxpagenumlimit + pagenumlimit);
+      setMinpagenumlimit(minpagenumlimit + pagenumlimit);
+    }
     console.log(currentpage)
   }
 
   return (
     <div>
       <Nav_Header/>
-      <Pagenumber totalpagenum={totalpagenum} perpage={perpage} paginate={paginate} decrease={decrease} increase={increase} />
+      <Pagenumber totalpagenum={totalpagenum} perpage={perpage} currentpage={currentpage} paginate={paginate} decrease={decrease} increase={increase}
+                  pagenumlimit={pagenumlimit} maxpagenumlimit={maxpagenumlimit} minpagenumlimit={minpagenumlimit} />
       <Content
         img={data[0] && data[0].user.avatar_url}
         content_title={data[0] && data[0].title}
--- src/components/Pagenumber.js
+++ src/components/Pagenumber.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pagination from 'react-bootstrap/Pagination'
-import PageItem from 'react-bootstrap/PageItem'
 
-const Pagenumber = ({totalpagenum,perpage, paginate,decrease,increase}) => {
+
+const Pagenumber = ({totalpagenum,perpage, paginate,decrease,increase,currentpage,pagenumlimit,maxpagenumlimit,minpagenumlimit}) => {
     const pageNum = [];
+    let active = currentpage ;
     for(let i=1; i<=Math.ceil(totalpagenum/perpage);i++){
         pageNum.push(i);
     }
@@ -15,7 +16,10 @@ const Pagenumber = ({totalpagenum,perpage, paginate,decrease,increase}) => {
             <Pagination.First onClick={()=> paginate(1)} />
             <Pagination.Prev onClick={()=> decrease()}/>
             {pageNum.map((number)=>{
-                return <Pagination.Item onClick={()=> paginate(number)}>{number}</Pagination.Item>}
+                if (number <= maxpagenumlimit && number > minpagenumlimit){
+                    return <Pagination.Item key={number} active={number === active} onClick={()=> paginate(number)}>{number}</Pagination.Item>}
+                else {return null}
+                }
                 )}
             <Pagination.Next onClick={()=> increase()}/>
             <Pagination.Last onClick={()=> paginate(Math.ceil(totalpagenum/perpage))} />
